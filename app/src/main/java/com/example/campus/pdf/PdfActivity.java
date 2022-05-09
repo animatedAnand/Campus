3399fa9065a9144f4aff217c8e57ae9d86de1d98
anand <kumar.abhayanand19@gmail.com>
2022-05-09 15:49:21 +0530

    Shimmer animatation added in pdf activity

--- app/src/main/java/com/example/campus/pdf/PdfActivity.java
+++ app/src/main/java/com/example/campus/pdf/PdfActivity.java
@@ -7,10 +7,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.campus.R;
 import com.example.campus.ui.gallery.GalleryAdapter;
+import com.facebook.shimmer.ShimmerFrameLayout;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,14 +27,30 @@ public class PdfActivity extends AppCompatActivity {
     private DatabaseReference reference;
     private ArrayList<PdfData>list;
     private PdfAdapter adapter;
+    private ShimmerFrameLayout shimmer_layout;
+    private LinearLayout ll_shimmer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf);
         rv_pdf=findViewById(R.id.rv_pdf);
+        shimmer_layout=findViewById(R.id.shimmer_view_container);
+        ll_shimmer=findViewById(R.id.ll_shimmer);
         reference= FirebaseDatabase.getInstance().getReference().child("Files");
         getPdfs();
+    }
+
+    @Override
+    protected void onPostResume() {
+        shimmer_layout.startShimmer();
+        super.onPostResume();
+    }
+
+    @Override
+    protected void onPause() {
+        shimmer_layout.stopShimmer();
+        super.onPause();
     }
 
     private void getPdfs() {
@@ -48,6 +67,8 @@ public class PdfActivity extends AppCompatActivity {
                 rv_pdf.setLayoutManager(new LinearLayoutManager(PdfActivity.this));
                 adapter=new PdfAdapter(list,PdfActivity.this);
                 rv_pdf.setAdapter(adapter);
+                shimmer_layout.stopShimmer();
+                ll_shimmer.setVisibility(View.GONE);
             }
 
             @Override
@@ -56,5 +77,6 @@ public class PdfActivity extends AppCompatActivity {
                 Toast.makeText(PdfActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 }
